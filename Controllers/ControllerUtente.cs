3b1b4b53baf64c704baed8fd7c3cd730901a6ad6
Luca <luca.sotgia@studenti.unicam.it>
2024-10-31 13:19:55 +0100

    Finita implementazione di tutti i Controller e Service

--- Controllers/ControllerUtente.cs
+++ Controllers/ControllerUtente.cs
@@ -1,13 +1,44 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ProgEntLib.DTO;
+using ProgEntLib.Service;
 
 namespace ProgEntLib.Controllers
 {
     [Authorize]
     [ApiController]
     [Route("api/[utente]")]
-    public class ControllerUtente
+    public class ControllerUtente : ControllerBase
     {
+        private readonly UtenteService _utenteService;
 
+        public ControllerUtente(UtenteService _utenteService)
+        {
+            _utenteService = this._utenteService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] DTOUtente utente)
+        {
+            var success = await _utenteService.CreaUtenteAsync(utente);
+            if (success == null)
+            {
+                return BadRequest("Nessun nuovo utente registrato!!");
+            }
+
+            return Ok(success);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] DTOLogin login)
+        {
+            var token = await _utenteService.AutenticaUtenteAsync(login);
+            if (token == null)
+            {
+                return Unauthorized("Invalid email or password!!");
+            }
+
+            return Ok(new { Token = token });
+        }
     }
 }
---
+++ Service/UtenteService.cs
@@ -0,0 +1,71 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using ProgEntLib.DTO;
+using ProgEntLib.Models;
+
+namespace ProgEntLib.Service
+{
+    public class UtenteService
+    {
+        private readonly IMongoCollection<Utente> _utentiCollection;
+        private readonly IConfiguration _configuration;
+
+        public UtenteService(IMongoDatabase database, IConfiguration configuration)
+        {
+            _utentiCollection = database.GetCollection<Utente>("users");
+            _configuration = configuration;
+        }
+
+        public async Task<Utente> CreaUtenteAsync(DTOUtente utente)
+        {
+            var existingUser = await _utentiCollection.Find(u => u.Email == utente.Email).FirstOrDefaultAsync();
+            if (existingUser != null)
+            {
+                return null;
+            }
+
+            utente.Password = BCrypt.Net.BCrypt.HashPassword(utente.Password);
+
+            var trueUser = new Utente
+            {
+                Nome = utente.Nome,
+                Cognome = utente.Cognome,
+                Email = utente.Email,
+                Password = utente.Password
+            }; 
+
+            await _utentiCollection.InsertOneAsync(trueUser);
+            return trueUser;
+        }
+
+        public async Task<string?> AutenticaUtenteAsync(DTOLogin login)
+        {
+            var user = await _utentiCollection.Find(u => u.Email == login.Email).FirstOrDefaultAsync();
+            if (user == null || !BCrypt.Net.BCrypt.Verify(user.Password, login.Password))
+            {
+                return null;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["JwtSettings:Secretkey"]);
+            var TokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier,user.Nome),
+                    new Claim(ClaimTypes.GivenName, user.Cognome),
+                    new Claim(ClaimTypes.Email, user.Email)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(TokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
